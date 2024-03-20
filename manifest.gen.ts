e8f40c8cc3ab8926ecbcc1186d29eb82c75bc375
Marcos Candeia <candeia@deco.cx>
2024-03-20 22:09:55 +0000

    Release draft environment with 3 changes made by Ana Carolina Duarte, Marcos Candeia

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -2,29 +2,29 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $$$$$$$$$$$0 from "./apps/decohub.ts";
-import * as $$$$$$$$$$$1 from "./apps/site.ts";
-import * as $$$$$$0 from "./sections/Footer.tsx";
-import * as $$$$$$1 from "./sections/Hero.tsx";
-import * as $$$$$$5 from "./sections/myHero.tsx";
-import * as $$$$$$2 from "./sections/ParagraphList.tsx";
-import * as $$$$$$3 from "./sections/SimpleTextList.tsx";
-import * as $$$$$$4 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$$$$$$$0 from "./apps/decohub.ts";
+import * as $$$$$$$$$$$$1 from "./apps/site.ts";
+import * as $$$$$$$0 from "./sections/Footer.tsx";
+import * as $$$$$$$1 from "./sections/Hero.tsx";
+import * as $$$$$$$5 from "./sections/myHero.tsx";
+import * as $$$$$$$2 from "./sections/ParagraphList.tsx";
+import * as $$$$$$$3 from "./sections/SimpleTextList.tsx";
+import * as $$$$$$$4 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "sections": {
-    "deco-sites/start/sections/Footer.tsx": $$$$$$0,
-    "deco-sites/start/sections/Hero.tsx": $$$$$$1,
-    "deco-sites/start/sections/myHero.tsx": $$$$$$5,
-    "deco-sites/start/sections/ParagraphList.tsx": $$$$$$2,
-    "deco-sites/start/sections/SimpleTextList.tsx": $$$$$$3,
-    "deco-sites/start/sections/Theme/Theme.tsx": $$$$$$4,
+    "deco-sites/landing/sections/Footer.tsx": $$$$$$$0,
+    "deco-sites/landing/sections/Hero.tsx": $$$$$$$1,
+    "deco-sites/landing/sections/myHero.tsx": $$$$$$$5,
+    "deco-sites/landing/sections/ParagraphList.tsx": $$$$$$$2,
+    "deco-sites/landing/sections/SimpleTextList.tsx": $$$$$$$3,
+    "deco-sites/landing/sections/Theme/Theme.tsx": $$$$$$$4,
   },
   "apps": {
-    "deco-sites/start/apps/decohub.ts": $$$$$$$$$$$0,
-    "deco-sites/start/apps/site.ts": $$$$$$$$$$$1,
+    "deco-sites/landing/apps/decohub.ts": $$$$$$$$$$$$0,
+    "deco-sites/landing/apps/site.ts": $$$$$$$$$$$$1,
   },
-  "name": "deco-sites/start",
+  "name": "deco-sites/landing",
   "baseUrl": import.meta.url,
 };
 
