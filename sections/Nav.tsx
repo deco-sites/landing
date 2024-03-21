9c277ea07844ea55d8f0a56eaf01901421c7a3b8
devartes <anacavalcante731@gmail.com>
2024-03-21 15:30:01 -0300

    refactor: improve Nav

--- sections/Nav.tsx
+++ sections/Nav.tsx
@@ -1,6 +1,17 @@
 import Image from "apps/website/components/Image.tsx";
 import type { ImageWidget } from "apps/admin/widgets.ts";
 
+type Type = "dark" | "light";
+
+type Button = {
+  label?: string;
+  url?: string;
+  style?: {
+    color?: Type;
+    border?: "yes" | "no";
+  };
+};
+
 export interface Nav {
   logo: {
     src?: ImageWidget;
@@ -11,26 +22,44 @@ export interface Nav {
       label?: string;
       url?: string;
     }[];
-    buttons?: {
-      label?: string;
-      url?: string;
-      style?: {
-        background?: Color;
-        color?: "text-base-content" | "text-base-100";
-        border?: "yes" | "no";
-      };
-    }[];
+    buttons?: Button[];
   };
 }
 
-type Color =
-  | "dark"
-  | "light";
-
-export const ColorType: Record<Color, string> = {
-  "dark": "bg-base-content",
-  "light": "bg-base-100",
+export const ColorType: Record<Type, string> = {
+  "dark": "base-content",
+  "light": "base-100",
 };
+
+export const StyleType: Record<"background" | "color", string> = {
+  "background": "bg-",
+  "color": "text-",
+};
+
+function getButtonStyle(button: Button) {
+  const baseStyle = "px-6 py-3";
+  const color = button.style?.color || "dark";
+  const combinedStyle = `${StyleType["background"]}${ColorType[color]} ${
+    StyleType["color"]
+  }${ColorType[color === "dark" ? "light" : "dark"]}`;
+  const borderStyle = button.style?.border === "yes"
+    ? "border-2 border-base-content rounded-xl"
+    : "rounded-xl border-2 border-transparent";
+
+  return `${baseStyle} ${combinedStyle} ${borderStyle}`.trim();
+}
+
+const generateLineStyles = (position: string) => `
+  absolute ${position} z-50 block h-0.5 w-7 bg-black transition-all duration-200 ease-out 
+`;
+
+const lineStyles = [
+  generateLineStyles("top-[-0.7rem]") +
+  "peer-checked:translate-y-[0.7rem] peer-checked:rotate-[45deg]",
+  generateLineStyles("top-[-0.35rem]") + "peer-checked:opacity-0",
+  generateLineStyles("top-[0]") +
+  "peer-checked:-translate-y-[0.2rem] peer-checked:-rotate-[45deg]",
+];
 
 export default function Nav({
   logo: {
@@ -52,13 +81,55 @@ export default function Nav({
   } = {},
 }: Nav) {
   return (
-    <nav class="flex items-center gap-8 px-16 py-4">
+    <nav class="flex gap-8 items-center justify-between px-16 py-4">
       <Image src={src || ""} width={100} height={28} alt={alt} />
-      <ul class="flex items-center justify-between w-full">
+
+      <label
+        class="cursor-pointer lg:hidden pt-6 px-3 relative z-40"
+        for="menu-mobile"
+      >
+        <input class="hidden peer" type="checkbox" id="menu-mobile" />
+        {lineStyles.map((style, index) => (
+          <div key={index} class={`relative ${style}`}></div>
+        ))}
+        <div class="backdrop-blur-sm bg-black/50 fixed h-full hidden inset-0 peer-checked:block w-full z-40">
+          &nbsp;
+        </div>
+        <div class="duration-500 fixed h-full overflow-y-auto overscroll-y-none peer-checked:translate-x-0 right-0 top-0 transition translate-x-full w-full z-40">
+          <div class="bg-white flex flex-col float-right gap-8 min-h-full pt-12 px-6 shadow-2xl w-1/2">
+            <ul class="flex flex-col gap-8">
+              {links.map((link) => (
+                <li>
+                  <a href={link.url} aria-label={link.label}>
+                    {link.label}
+                  </a>
+                </li>
+              ))}
+            </ul>
+            <ul class="flex flex-col gap-8">
+              {buttons.map((button) => (
+                <li>
+                  <a
+                    href={button.url}
+                    class={getButtonStyle(button)}
+                    aria-label={button.label}
+                  >
+                    {button.label}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </label>
+
+      <ul class="hidden items-center justify-between lg:flex w-full">
         <ul class="flex gap-8">
           {links.map((link) => (
             <li>
-              <a href={link.url}>{link.label}</a>
+              <a href={link.url} aria-label={link.label}>
+                {link.label}
+              </a>
             </li>
           ))}
         </ul>
@@ -67,15 +138,8 @@ export default function Nav({
             <li>
               <a
                 href={button.url}
-                class={`px-6 py-3 ${
-                  button.style?.border
-                    ? ColorType[button.style?.background || "dark"]
-                    : ""
-                } ${button.style?.color} ${
-                  button.style?.border === "yes"
-                    ? "border-2 border-base-content rounded-full"
-                    : ""
-                }`}
+                class={getButtonStyle(button)}
+                aria-label={button.label}
               >
                 {button.label}
               </a>
