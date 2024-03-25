1568666d32bb2a6df31fafa7abe40f2b38819c11
devartes <anacavalcante731@gmail.com>
2024-03-25 13:43:15 -0300

    refactor: improve Testimonials

--- sections/Testimonials.tsx
+++ sections/Testimonials.tsx
@@ -8,8 +8,8 @@ import Image from "apps/website/components/Image.tsx";
 /**
  * @titleBy alt
  */
-export interface Slider {
-  action?: {
+export interface Testimonial {
+  content?: {
     description?: string;
     avatar?: ImageWidget;
     /** @description Image's alt text */
@@ -21,11 +21,7 @@ export interface Slider {
 
 export interface Props {
   title?: string;
-  images?: Slider[];
-  /**
-   * @description Check this option when this Slider is the biggest image on the screen for image optimizations
-   */
-  preload?: boolean;
+  slides?: Testimonial[];
   /**
    * @title Show arrows
    * @description show arrows to navigate through the images
@@ -45,9 +41,9 @@ export interface Props {
 
 const DEFAULT_PROPS = {
   title: "This is where you'll put your customer testimonials",
-  images: [
+  slides: [
     {
-      action: {
+      content: {
         description:
           "Showcase customer feedback that emphasizes your product or service's key features and addresses prospective clients' concerns. Display endorsements from customer groups that mirror your target audience.",
         avatar:
@@ -58,7 +54,7 @@ const DEFAULT_PROPS = {
       },
     },
     {
-      action: {
+      content: {
         description:
           "Showcase customer feedback that emphasizes your product or service's key features and addresses prospective clients' concerns. Display endorsements from customer groups that mirror your target audience.",
         avatar:
@@ -69,7 +65,7 @@ const DEFAULT_PROPS = {
       },
     },
     {
-      action: {
+      content: {
         description:
           "Showcase customer feedback that emphasizes your product or service's key features and addresses prospective clients' concerns. Display endorsements from customer groups that mirror your target audience.",
         avatar:
@@ -79,35 +75,56 @@ const DEFAULT_PROPS = {
         position: "Position, Company name",
       },
     },
-  ],
-  preload: true,
+    {
+      content: {
+        description:
+          "Showcase customer feedback that emphasizes your product or service's key features and addresses prospective clients' concerns. Display endorsements from customer groups that mirror your target audience.",
+        avatar:
+          "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/1527/7286de42-e9c5-4fcb-ae8b-b992eea4b78e",
+        alt: "Avatar",
+        name: "Name Surname",
+        position: "Position, Company name",
+      },
+    },
+    {
+      content: {
+        description:
+          "Showcase customer feedback that emphasizes your product or service's key features and addresses prospective clients' concerns. Display endorsements from customer groups that mirror your target audience.",
+        avatar:
+          "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/1527/7286de42-e9c5-4fcb-ae8b-b992eea4b78e",
+        alt: "Avatar",
+        name: "Name Surname",
+        position: "Position, Company name",
+      },
+    },
+  ]
 };
 
 function SliderItem(
-  { image, id }: { image: Slider; id: string },
+  { slide, id }: { slide: Testimonial; id: string },
 ) {
   const {
-    action,
-  } = image;
+    content,
+  } = slide;
 
   return (
     <div
       id={id}
       class="relative overflow-y-hidden w-full min-h-[292px]"
     >
-      <div class="flex flex-col justify-center gap-16 p-8 border border-black rounded-2xl h-full max-w-[600px]">
-        <p class="text-lg">{action?.description}</p>
+      <div class="flex flex-col justify-center gap-16 p-8 border border-base-content rounded-2xl h-full max-w-[600px]">
+        <p class="text-lg">{content?.description}</p>
         <div class="flex items-center gap-5">
           <Image
             class="object-cover w-14 h-14 rounded-full"
-            alt={action?.alt}
-            src={action?.avatar || ""}
+            alt={content?.alt}
+            src={content?.avatar || ""}
             width={56}
             height={56}
           />
           <div class="flex flex-col">
-            <p class="font-semibold text-base">{action?.name}</p>
-            <p class="text-base">{action?.position}</p>
+            <p class="font-semibold text-base">{content?.name}</p>
+            <p class="text-base">{content?.position}</p>
           </div>
         </div>
       </div>
@@ -115,7 +132,7 @@ function SliderItem(
   );
 }
 
-function Dots({ images, interval = 0 }: Props) {
+function Dots({ slides, interval = 0 }: Props) {
   return (
     <>
       <style
@@ -129,13 +146,13 @@ function Dots({ images, interval = 0 }: Props) {
           `,
         }}
       />
-      <ul class="carousel col-span-full gap-6 z-10">
-        {images?.map((_, index) => (
+      <ul class="carousel col-span-full gap-3 z-10">
+        {slides?.map((_, index) => (
           <li class="carousel-item">
             <Slider.Dot index={index}>
               <div class="py-5">
                 <div
-                  class="w-2 h-2 rounded-full group-disabled:animate-progress bg-gradient-to-r from-base-100 from-[length:var(--dot-progress)] to-[#E4E4E7] to-[length:var(--dot-progress)]"
+                  class="w-2 h-2 rounded-full group-disabled:animate-progress dot"
                   style={{ animationDuration: `${interval}s` }}
                 />
               </div>
@@ -155,7 +172,7 @@ function Buttons() {
           <Icon
             class="text-base-content"
             size={24}
-            id="ChevronLeft"
+            id="ArrowRight"
             strokeWidth={3}
           />
         </Slider.PrevButton>
@@ -165,7 +182,7 @@ function Buttons() {
           <Icon
             class="text-base-content"
             size={24}
-            id="ChevronRight"
+            id="ArrowLeft"
             strokeWidth={3}
           />
         </Slider.NextButton>
@@ -174,21 +191,23 @@ function Buttons() {
   );
 }
 
-function Testimonials(props: Props) {
+function Carousel(props: Props) {
   const id = useId();
-  const { title, images, preload, interval } = { ...DEFAULT_PROPS, ...props };
+  const { title, slides, interval } = { ...DEFAULT_PROPS, ...props };
 
   return (
     <div
       id={id}
-      class="min-h-min flex flex-col"
+      class="min-h-min flex flex-col lg:container md:max-w-6xl md:mx-auto"
     >
-      <h2 class="lg:text-5xl text-3xl pl-6 lg:pl-16 lg:w-1/2 pb-12 lg:pb-20">{title}</h2>
+      <h2 class="lg:text-5xl text-3xl pl-6 lg:pl-16 lg:w-1/2 pb-12 lg:pb-20">
+        {title}
+      </h2>
       <Slider class="carousel carousel-center w-full col-span-full row-span-full gap-6 px-6 lg:px-16">
-        {images?.map((image, index) => {
+        {slides?.map((slide, index) => {
           let params = {};
-          if (image.action && "description" in image.action) {
-            params = { promotion_name: image.action.alt };
+          if (slide.content && "description" in slide.content) {
+            params = { promotion_name: slide.content.alt };
           }
           return (
             <Slider.Item
@@ -196,7 +215,7 @@ function Testimonials(props: Props) {
               class="carousel-item max-w-[600px] w-full"
             >
               <SliderItem
-                image={image}
+                slide={slide}
                 id={`${id}::${index}`}
               />
             </Slider.Item>
@@ -205,7 +224,7 @@ function Testimonials(props: Props) {
       </Slider>
 
       <div class="flex justify-between pt-8 px-6 lg:px-16">
-        {props.dots && <Dots images={images} interval={interval} />}{" "}
+        {props.dots && <Dots slides={slides} interval={interval} />}{" "}
         {props.arrows && <Buttons />}
       </div>
 
@@ -214,4 +233,4 @@ function Testimonials(props: Props) {
   );
 }
 
-export default Testimonials;
+export default Carousel;
