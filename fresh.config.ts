4d0ca92c26fbccc1a0c3eef39272429dd8a23a48
gimenes <tlgimenes@gmail.com>
2024-03-27 17:43:58 -0300

    upgrade config and tailwind plugin

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from "$fresh/server.ts";
-import plugins from "https://denopkg.com/deco-sites/std@1.22.0/plugins/mod.ts";
-import partytownPlugin from "partytown/mod.ts";
-import decoManifest from "./manifest.gen.ts";
+import plugins from "https://denopkg.com/deco-sites/std@1.25.0/plugins/mod.ts";
+import manifest from "./manifest.gen.ts";
+import tailwind from "./tailwind.config.ts";
 
 export default defineConfig({
-  plugins: [
-    ...plugins({
-      manifest: decoManifest,
-    }),
-    partytownPlugin(),
-  ],
+  plugins: plugins({
+    manifest,
+    // deno-lint-ignore no-explicit-any
+    tailwind: tailwind as any,
+  }),
 });
